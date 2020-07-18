d506c45057dbc03897bbafce6fb7563508f2aca3
Venetronic <contacto@venetronic.com>
2020-07-18 12:38:23 -0500

    Updates

---
+++ precache-manifest.18516387667a681159202a808221ca32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff1e134ff6637e9f7ebff02eff2780c0",
+    "url": "/testItMakers/index.html"
+  },
+  {
+    "revision": "4eb6956902c804c4145b",
+    "url": "/testItMakers/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dde4c89ee82752285d39",
+    "url": "/testItMakers/static/js/2.19a72569.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/testItMakers/static/js/2.19a72569.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4eb6956902c804c4145b",
+    "url": "/testItMakers/static/js/main.f8fd7007.chunk.js"
+  },
+  {
+    "revision": "b7ea26d726ad3de2d6d1",
+    "url": "/testItMakers/static/js/runtime-main.77f00fb1.js"
+  }
+]);
